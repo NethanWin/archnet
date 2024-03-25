8db0f0d47318162cc601172404ae1fddac389e57
NethanWin <127036972+NethanWin@users.noreply.github.com>
2024-03-25 18:45:26 +0200

    Add lazyvim

--- archnet.sh
+++ archnet.sh
@@ -66,6 +66,17 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+
 
 
 # pacman -S iwd
